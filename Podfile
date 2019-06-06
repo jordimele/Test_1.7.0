8c4ae9464c41d83c15c7e8ede5755a5003bf2280
Jordi Melé Badia <jmele@opentrends.net>
2019-06-06 07:10:39 +0200

    Create test project, 1.4.0

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Test_1.7.0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Test_1.7.0
+  platform :ios, '9.0'
+  pod 'RealmSwift', '3.10.0'
+
+end
---
+++ Test_1.7.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_1.7.0
+//
+//  Created by Jordi Melé Badia on 06/06/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
